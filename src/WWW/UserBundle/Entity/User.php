5dba72f9a4d248070e33ea501333c6992549d8a9
Barbeitos <Barbeitos@XPS15>
2016-10-17 11:54:18 +0200

    Creando variables de User Task #WWW-2 - Crear UserBundle

--- src/WWW/UserBundle/Entity/User.php
+++ src/WWW/UserBundle/Entity/User.php
@@ -17,6 +17,46 @@ class User extends BaseUser
      * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
+    
+    /**
+     * @ORM\Column(type="string", length=100, columnDefinition="VARCHAR(100)")
+     */
+    private $nombre;
+    
+    /**
+     * @ORM\Column(type="string", length=255, columnDefinition="VARCHAR(255)")
+     */
+    private $apellidos;
+    
+    /**
+     * @ORM\Column(type="string", length=1, columnDefinition="CHAR(1)")
+     */
+    private $sexo;
+    
+    /**
+     * @ORM\Column(type="date", name = "fecha_nacimiento")
+     */
+    private $fechaNacimiento;
+    
+    /**
+     * @ORM\Column(type="string", length=255, columnDefinition="VARCHAR(255)")
+     */
+    private $linkInvitacion;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $anfitrion;
+    
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $invitados;
+    
+    /**
+     * @ORM\Column(type="string", length=10, columnDefinition="VARCHAR(10))
+     */
+    private $cp;
 
     public function __construct()
     {
