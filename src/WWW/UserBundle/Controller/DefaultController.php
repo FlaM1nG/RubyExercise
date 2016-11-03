0edf9c541c5ca74183352a154e2fc28065bb97c4
Carlos Andres Moron Granados <carlosamg87@gmail.com>
2016-11-03 08:21:31 +0100

    Agregado el loggin a falta de la encriptacion

--- src/WWW/UserBundle/Controller/DefaultController.php
+++ src/WWW/UserBundle/Controller/DefaultController.php
@@ -15,8 +15,45 @@ class DefaultController extends Controller
     }
     
     public function loginAction(Request $request){
+         if($request->getMethod()=="POST")
+        {
+            $email=$request->get("email");
+            $password=$request->get("password");  
+         
+            //echo "correo=".$correo."<br>pass=".$pass;exit;
+            $user=$this->getDoctrine()->getRepository('UserBundle:User')->findOneBy(array("email"=>$email,"password"=>$password));
+        
+            if($user)
+            {
+             $session=$request->getSession();
+               $session->set("id",$user->getId());
+               $session->set("username",$user->getUsername());
+               //echo $session->get("username");exit;
+               return $this->redirect($this->generateUrl('user_homepage'));
+            }else
+            {
+                $this->get('session')->getFlashBag()->add(
+                                'mensaje',
+                                'Los datos ingresados no son válidos'
+                            );
+                    return $this->redirect($this->generateUrl('user_login'));
+            }
+        }
+        
+        return $this->render('UserBundle:Default:login.html.twig');
         
     }
+    
+    public function logoutAction(Request $request){
+        $session=$request->getSession();
+        $session->clear();
+         $this->get('session')->getFlashBag()->add(
+                                'mensaje',
+                                'Se ha cerrado la sesion con exito'
+                            );
+                    return $this->redirect($this->generateUrl('user_login'));
+    }
+    
     
     public function showRegisterAction(){
         
