07033380da5b9e3ab27a89cf12942eb8c0d2ff58
Hozarberto <Hozarberto@DESKTOP-7E5AC4Q>
2016-11-24 15:40:36 +0100

    Cambiados unos pocos campos Task #WWW-31 - Crear Tabla Service

--- src/WWW/OthersBundle/Entity/Trade.php
+++ src/WWW/OthersBundle/Entity/Trade.php
@@ -305,4 +305,60 @@ class Trade
     {
         return $this->category;
     }
+    /**
+     * @var float
+     */
+    private $longitude;
+
+    /**
+     * @var float
+     */
+    private $latitude;
+
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     * @return Trade
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float 
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set latitude
+     *
+     * @param float $latitude
+     * @return Trade
+     */
+    public function setLatitude($latitude)
+    {
+        $this->latitude = $latitude;
+
+        return $this;
+    }
+
+    /**
+     * Get latitude
+     *
+     * @return float 
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
 }
