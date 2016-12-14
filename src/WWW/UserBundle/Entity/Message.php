16dc703d533db473fef0f43438149fd7f9b4d312
Hozarberto <Hozarberto@DESKTOP-7E5AC4Q>
2016-12-14 14:57:44 +0100

    Cambiadas algunas cosas

---
+++ src/WWW/UserBundle/Entity/Message.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace WWW\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Message
+ */
+class Message
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var int
+     */
+    private $status;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    /**
+     * @var \DateTime
+     */
+    private $revisionDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set subject
+     *
+     * @param string $subject
+     * @return Message
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Get subject
+     *
+     * @return string 
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * Set message
+     *
+     * @param string $message
+     * @return Message
+     */
+    public function setMessage($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    /**
+     * Get message
+     *
+     * @return string 
+     */
+    public function getMessage()
+    {
+        return $this->message;
+    }
+
+    /**
+     * Set status
+     *
+     * @param integer $status
+     * @return Message
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return integer 
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Message
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set revisionDate
+     *
+     * @param \DateTime $revisionDate
+     * @return Message
+     */
+    public function setRevisionDate($revisionDate)
+    {
+        $this->revisionDate = $revisionDate;
+
+        return $this;
+    }
+
+    /**
+     * Get revisionDate
+     *
+     * @return \DateTime 
+     */
+    public function getRevisionDate()
+    {
+        return $this->revisionDate;
+    }
+}
