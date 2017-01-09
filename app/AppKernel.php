f8c84a8ba8bbcbe45c0b0d191c6eb76a89fbe552
Julio <Julio@Julio-PC>
2017-01-09 15:58:06 +0100

    gestion arreglando cosillas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,7 +21,8 @@ class AppKernel extends Kernel
             new Gregwar\CaptchaBundle\GregwarCaptchaBundle(),
             new SGestion\AdminBundle\SGestionAdminBundle(),
             new WWW\ServiceBundle\ServiceBundle(),
-            new WWW\OthersBundle\OthersBundle()
+            new WWW\OthersBundle\OthersBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
