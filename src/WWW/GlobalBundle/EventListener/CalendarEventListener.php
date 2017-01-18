58f5034bcd3c74088c59e8b0cb7ae29fd57f0f5b
Barbeitos <Barbeitos@DESKTOP-337OV47>
2017-01-18 15:16:45 +0100

    Calendar Carlos

--- src/WWW/GlobalBundle/EventListener/CalendarEventListener.php
+++ src/WWW/GlobalBundle/EventListener/CalendarEventListener.php
@@ -32,7 +32,7 @@ class CalendarEventListener
         // load events using your custom logic here,
         // for instance, retrieving events from a repository
 
-        $companyEvents = $this->entityManager->getRepository('AcmeDemoBundle:MyCompanyEvents')
+        $companyEvents = $this->entityManager->getRepository('GlobalBundle:MyCompanyEvents')
                           ->createQueryBuilder('company_events')
                           ->where('company_events.event_datetime BETWEEN :startDate and :endDate')
                           ->setParameter('startDate', $startDate->format('Y-m-d H:i:s'))
