5eb02ba51cb4e716afb5af50f597cc8907e308f9
Hozarberto <Hozarberto@DESKTOP-7E5AC4Q>
2017-01-31 11:53:57 +0100

    Cambiadas algunas cosas

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new WWW\OthersBundle\OthersBundle(),
             new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new WWW\CarsBundle\CarsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
