6580ac957fb0a2bcf60b4bc77267313b8b3dd474
Carlos Andres Moron Granados <carlosamg87@gmail.com>
2017-02-24 09:10:28 +0100

    cambios en el javascript del calendario

--- web/js/calendario/calendar.js
+++ web/js/calendario/calendar.js
@@ -7,37 +7,7 @@ $(document).ready(function() {
     var m = date.getMonth();
     var y = date.getFullYear();
 
-    /*$('#calendar-holder').fullCalendar({
-        header: {
-            left: 'prev, next',
-            center: 'title',
-            right: 'month, basicWeek, basicDay,'
-        },
-        lazyFetching: true,
-        timeFormat: {
-            // for agendaWeek and agendaDay
-            agenda: 'h:mmt',    // 5:00 - 6:30
 
-            // for all other views
-            '': 'h:mmt'         // 7p
-        },
-        eventSources: [
-            {
-                url: Routing.generate('fullcalendar_loader'),
-                type: 'POST',
-                // A way to add custom filters to your event listeners
-                data: {
-                },
-                error: function() {
-                   //alert('There was an error while fetching Google Calendar!');
-                }
-            }
-        ]
-    });
-    */
-
-
-		
 		$('#calendar-holder').fullCalendar({
 
                        
