6b24d33a36982d4c64d27130abebe915470316ca
Rocio <Rocio@ro>
2017-03-07 16:39:42 +0100

    función para crear nuevas ofertas

--- src/WWW/HouseBundle/Controller/HouseOffersController.php
+++ src/WWW/HouseBundle/Controller/HouseOffersController.php
@@ -5,3 +5,16 @@
  * Date: 07/03/2017
  * Time: 15:59
  */
+
+namespace WWW\HouseBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HouseOffersController extends Controller
+{
+    public function createNewOfferAction(){
+        $this->render("HouseBundle::newOfferHouseRent.html.twig", array(
+                      'service' => 6
+        ));
+    }
+}
