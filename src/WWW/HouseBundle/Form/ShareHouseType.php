0fb08f34a4308888d6b9ce30954c5b95e39d0aa1
Rocio <Rocio@ro>
2017-03-08 12:22:53 +0100

    creación de formulario para crear ofertas de casas sin tener en cuenta el calendario

--- src/WWW/HouseBundle/Form/ShareHouseType.php
+++ src/WWW/HouseBundle/Form/ShareHouseType.php
@@ -8,7 +8,42 @@
 
 namespace WWW\HouseBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use WWW\ServiceBundle\Form\OfferType;
 
-class HouseRentType {
+class ShareHouseType extends AbstractType{
+
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        
+        $arrayHouses = $options['arrayHouses'];
+
+        $builder
+            ->add('house', ChoiceType::class, array('label' => 'Seleccione una casa',
+                                                    'choices' => $arrayHouses,
+                                                    'choice_label' => 'title',
+                                                    'choice_value' => 'id',
+                                                    'choices_as_values' => true,
+                                                    'data' => $options['data']->getHouse() ))
+            ->add('offer', OfferType::class, array('label' => ''))
+
+            ->add('price', MoneyType::class, array('label' => 'Precio base'))
+
+            ->add('newShareHouse', SubmitType::class, array('label' => 'Guardar'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver){
+
+        $resolver->setDefaults(array('data_class' => 'WWW\HouseBundle\Entity\ShareHouse',
+                                     'arrayHouses' => null));
+    }
+
+    public function getBlockPrefix(){
+        return 'shareHouse';
+    }
 
 }
