c50dcfe921a6eb08ec923dca0eb978becd00f90c
Rocio <Rocio@ro>
2017-03-15 15:04:47 +0100

    controlados para mostrar una oferta concreta, poder añadir comentarios y enviar mensajes

--- src/WWW/HouseBundle/Controller/HouseOffersController.php
+++ src/WWW/HouseBundle/Controller/HouseOffersController.php
@@ -16,6 +16,12 @@ use Symfony\Component\HttpFoundation\Request;
 use WWW\HouseBundle\Entity\ShareHouse;
 use WWW\HouseBundle\Entity\House;
 use WWW\HouseBundle\Form\ShareHouseType;
+use WWW\ServiceBundle\Entity\Comment;
+use WWW\ServiceBundle\Form\CommentType;
+use WWW\ServiceBundle\Form\OfferSuscribeType;
+use WWW\UserBundle\Entity\Message;
+use WWW\UserBundle\Form\MessageType;
+use WWW\UserBundle\Entity\User;
 
 
 class HouseOffersController extends Controller
@@ -92,10 +98,164 @@ class HouseOffersController extends Controller
 
     public function listOfferShareHouseAction(Request $request){
 
-        return $this->render('services/serHouseRents.html.twig');
+        $arrayOffers = $this->getOffersShareHouse();
+
+        return $this->render('services/serHouseRents.html.twig', array(
+                             'arrayOffers' => $arrayOffers
+        ));
     }
 
-    private function getOffersShareHouse(Request $request){
+    private function getOffersShareHouse() {
+
+        $file = MyConstants::PATH_APIREST . 'services/share_house/list_share_house.php';
+        $ch = new ApiRest();
+        $arrayOffers = null;
+
+        $data['service_id'] = 6;
+        $data['search'] = "";
+
+        $info['data'] = json_encode($data);
+
+        $result = $ch->resultApiRed($info, $file);
+
+        if ($result['result'] == 'ok')
+            $arrayOffers = $result['offers'];
+
+        return $arrayOffers;
+    }
+
+    public function showOfferShareHouseAction(Request $request){
+
+        $message = new Message();
+        $comment = new Comment();
+
+        $formSubscribe = $this->createForm(OfferSuscribeType::class);
+
+        $formComment = $this->createForm(CommentType::class, $comment);
+        $formComment->handleRequest($request);
         
+        if($formComment->isSubmitted() AND $formComment->isValid()):
+            $this->saveComment($request, $comment);
+            $formComment = $this->createForm(CommentType::class, new Comment());
+        endif;
+
+        $offerShareHouse = $this->getoffer($request);
+
+        $message = $this->fillMessage($request, $offerShareHouse);
+
+        $formMessage = $this->createForm(MessageType::class, $message);
+        $formMessage->handleRequest($request);
+
+        if($formMessage->isSubmitted()):
+            $this->sendMessage($request, $message, $offerShareHouse->getOffer()->getUserAdmin()->getUsername());
+            $m = $this->fillMessage($request, $offerShareHouse);
+            $formMessage = $this->createForm(MessageType::class, $m);
+        endif;
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = null;
+
+        if(!empty($offerShareHouse->getOffer()->getComments())):
+
+            $pagination = $paginator->paginate(
+                $offerShareHouse->getOffer()->getComments(),
+                $request->query->getInt('page', 1),
+                MyConstants::NUM_COMMENTS_PAGINATOR
+            );
+
+        endif;
+
+        return $this->render('offer/offHouseRents.html.twig', array(
+                             'offer' => $offerShareHouse,
+                             'arrayAttr' => $offerShareHouse->getArrayGroupsAttrH(),
+                             'formMessage' => $formMessage->createView(),
+                             'formComment' => $formComment->createView(),
+                             'formSubscribe' => $formSubscribe->createView(),
+                             'pagination' => $pagination,
+                             'numComment' => MyConstants::NUM_COMMENTS_PAGINATOR,
+                             'service' => 6
+        ));
+    }
+
+    private function getoffer(Request $request){
+
+        $file = MyConstants::PATH_APIREST.'services/share_house/get_share_house.php';
+        $ch = new ApiRest();
+        $offer = null;
+
+        $data['id'] = $request->get('idOffer');
+
+        $result = $ch->resultApiRed($data,$file);
+
+
+//        print_r($result);
+
+        $offer = new ShareHouse($result);
+        return $offer;
+
+    }
+
+    private function saveComment(Request $request, Comment $comment){
+
+        $ch = new ApiRest();
+        $ut = new Utilities();
+        $file = MyConstants::PATH_APIREST."services/inscription/comment.php";
+
+        $data['id'] = $request->getSession()->get('id');
+        $data['username'] = $request->getSession()->get('username');
+        $data['password'] = $request->getSession()->get('password');
+        $data['offer_id'] = $request->get('idOffer');
+        $data['comment'] = $comment->getComment();
+
+        $result = $ch->resultApiRed($data, $file);
+
+        if($result['result'] == 'ok'):
+            return $result['result'];
+        else:
+            $ut->flashMessage("comment", $request, $result);
+        endif;
+    }
+
+    private function fillMessage(Request $request, $shareHouse){
+
+        $message = new Message();
+
+        $user = new User();
+        $user->setId($request->getSession()->get('id'));
+        $user->setUsername($request->getSession()->get('username'));
+
+        $userTo = new User();
+        $userTo->setId($shareHouse->getOffer()->getUserAdmin()->getId());
+        $userTo->setUsername($shareHouse->getOffer()->getUserAdmin()->getUsername());
+
+        $message->setFrom($user);
+        $message->setTo($userTo);
+        $message->setSubject('Oferta: '.$shareHouse->getOffer()->getTitle());
+
+        return $message;
+    }
+
+    private function sendMessage(Request $request, Message $message, $to){
+
+        $ch = new ApiRest();
+        $file = MyConstants::PATH_APIREST."user/messages/send_message.php";
+        $ut = new Utilities();
+
+        $data['id'] = $request->getSession()->get('id');
+        $data['username'] = $request->getSession()->get('username');
+        $data['password'] = $request->getSession()->get('password');
+        $data['subject'] = $message->getSubject();
+        $data['message'] = $message->getMessage();
+        $data['offer'] = $request->get('idOffer');
+        $data['to'] = $to;
+
+        if(isset($request->get('message')['to'])):
+            $data['to'] = $request->get('message')['to'];
+        endif;
+
+        $result = $ch->resultApiRed($data, $file);
+
+        $ut->flashMessage("message", $request, $result);
+
     }
 }
