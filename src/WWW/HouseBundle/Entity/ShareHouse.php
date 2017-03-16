4f6ca65f7442f82eb027471e6e8130b6e239900d
Rocio <Rocio@ro>
2017-03-16 09:01:10 +0100

    añade al constructo las horas de entrada y salida cambio de sitio de los atributos nuevos

--- src/WWW/HouseBundle/Entity/ShareHouse.php
+++ src/WWW/HouseBundle/Entity/ShareHouse.php
@@ -35,8 +35,18 @@ class ShareHouse
      */
     private $price;
 
+    /**
+     * @var \DateTime
+     */
+    private $entryTime;
+
+    /**
+     * @var \DateTime
+     */
+    private $departureTime;
+    
+
     public function __construct($arrayData = null) {
-//        $this->house = new House();
 
         if(gettype($arrayData)== 'array' AND !empty($arrayData)):
             if(array_key_exists('house', $arrayData)):
@@ -47,6 +57,10 @@ class ShareHouse
             endif;
             $this->price = $arrayData['price'];
             $this->offer = new Offer($arrayData);
+            $this->departureTime = \DateTime::createFromFormat('H:i:s', $arrayData['departure_time']);
+            $this->entryTime = \DateTime::createFromFormat('H:i:s', $arrayData['entry_time']);
+        else:
+            $this->house = new House();
         endif;
 
     }
@@ -153,16 +167,6 @@ class ShareHouse
     {
         return $this->price;
     }
-    /**
-     * @var \DateTime
-     */
-    private $entryTime;
-
-    /**
-     * @var \DateTime
-     */
-    private $departureTime;
-
 
     /**
      * Set entryTime
