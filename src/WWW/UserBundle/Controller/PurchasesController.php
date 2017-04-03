f90cb616698d1918cff5c1d4b046d414e7c307c0
Rocio <Rocio@ro>
2017-04-03 11:45:44 +0200

    funcionalidad para oferta disfrutada

--- src/WWW/UserBundle/Controller/PurchasesController.php
+++ src/WWW/UserBundle/Controller/PurchasesController.php
@@ -48,14 +48,16 @@ class PurchasesController extends Controller{
     private function getPurchases(Request $request){
         $file = MyConstants::PATH_APIREST."services/inscription/get_inscriptions.php";
         $ch = new ApiRest();
-        
+
         $data['username'] = $request->getSession()->get('username');
         $data['id'] = $request->getSession()->get('id');
         $data['password'] = $request->getSession()->get('password');
         
         $result = $ch->resultApiRed($data, $file);
-        
-        return $result['inscriptions'];
+
+        if($result['result'] == 'ok')
+            return $result['inscriptions'];
+        else return null;
     }
     
     private function getOffer(Request $request){
@@ -82,5 +84,35 @@ class PurchasesController extends Controller{
                               'urlImage' => $result[0]['photo_url'],
                               'valorated_by_me'=> $result [0]['valorated_by_me']
                 ));
+    }
+
+    public function offerEnjoyedAction(Request $request){
+
+        $file = MyConstants::PATH_APIREST.'services/inscription/transition.php';
+        $ch = new ApiRest();
+        $response = new JsonResponse();
+
+        $idOffer = $request->get('idOffer');
+
+        $data['id'] = $request->getSession()->get('id');
+        $data['username'] = $request->getSession()->get('username');
+        $data['password'] = $request->getSession()->get('password');
+        $data['user_id'] = $data['id'];
+        $data['offer_id'] = $idOffer;
+        $data['status'] = 5;
+
+        $result = $ch->resultApiRed($data, $file);
+
+        if($result['result'] == 'ok'):
+
+            $response->setData(array('result' => 'ok',
+                                     'message' => 'Nos alegramos de que la haya disfrutado'));
+        else:
+            $response->setData(array('result' => 'ko',
+                                     'message' => 'Se ha producido un error, por favor vuelva a intentarlo más tarde'));
+        endif;
+
+        return $response;
+
     }
 }
