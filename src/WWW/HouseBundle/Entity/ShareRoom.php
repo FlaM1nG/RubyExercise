6029db9e6757f88f7defc1e0cd1b01936a230f68
Rocio <Rocio@ro>
2017-04-04 10:28:09 +0200

    creación constructor Cambio de sitio del atribute offer, se pone al princpio con los demás

--- src/WWW/HouseBundle/Entity/ShareRoom.php
+++ src/WWW/HouseBundle/Entity/ShareRoom.php
@@ -3,6 +3,8 @@
 namespace WWW\HouseBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Util\Inflector as Inflector;
+use WWW\ServiceBundle\Entity\Offer;
 
 /**
  * ShareRoom
@@ -39,6 +41,24 @@ class ShareRoom
      */
     private $country;
 
+    /**
+     * @var \WWW\ServiceBundle\Entity\Offer
+    */
+    private $offer;
+
+    public function __construct($data){
+
+        foreach($data as $key => $value):
+            $keyCamelize = Inflector::camelize($key);
+
+            if(property_exists("WWW\HouseBundle\Entity\ShareRoom", $keyCamelize)):
+                $this->$keyCamelize = $value;
+            endif;
+
+        endforeach;
+
+        $this->offer = new Offer($data);
+    }
 
     /**
      * Get id
@@ -164,11 +184,6 @@ class ShareRoom
     {
         return $this->country;
     }
-    /**
-     * @var \WWW\ServiceBundle\Entity\Offer
-     */
-    private $offer;
-
 
     /**
      * Set offer
