c5826ab823d8cc7c96faba7d0ab5d5c816d959e1
DESKTOP-C2860ND\Barbeitos <danielbarbeitos@gmail.com>
2017-06-06 14:52:05 +0200

    Google Analytics
    Añadido eventos de click para controlar la pulsación del botón de radio y registro.

---
+++ web/js/googleAnalytics/cabecera.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    ga('send', 'event', {
+        eventCategory: 'Click Radio',
+        eventAction: 'click',
+        eventLabel: '#eventRadio'
+    });
+
+    ga('send', 'event', {
+        eventCategory: 'Click Registro',
+        eventAction: 'click',
+        eventLabel: '#eventRegistrarse'
+    });
+});
