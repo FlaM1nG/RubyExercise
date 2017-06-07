46ca11593b4ffdb5322989e476405fc1ef8f3ce6
DESKTOP-C2860ND\Barbeitos <danielbarbeitos@gmail.com>
2017-06-07 10:31:00 +0200

    Google Analytics para la parte del menu movil

--- web/js/googleAnalytics/cabecera.js
+++ web/js/googleAnalytics/cabecera.js
@@ -2,14 +2,33 @@ $("#eventRadio").on("click", function () {
     ga('send', 'event', {
         eventCategory: 'Click Radio',
         eventAction: 'click',
-        eventLabel: '#eventRadio'
+        eventLabel: 'Click en el botón de la radio'
     });
 });
+
 
 $("#eventRegistrarse").on("click", function () {
     ga('send', 'event', {
         eventCategory: 'Click Registro',
         eventAction: 'click',
-        eventLabel: '#eventRegistrarse'
+        eventLabel: 'Click en el botón de registrarse'
+    });
+});
+
+
+$("#eventRadioMovil").on("click", function () {
+    ga('send', 'event', {
+        eventCategory: 'Click Radio',
+        eventAction: 'click',
+        eventLabel: 'Click en el botón de la radio menu movil'
+    });
+});
+
+
+$("#eventRegistrarseMovil").on("click", function () {
+    ga('send', 'event', {
+        eventCategory: 'Click Registro',
+        eventAction: 'click',
+        eventLabel: 'Click en el botón de registrarse menu movil'
     });
 });
